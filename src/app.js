866f8043a5921dbaf3fbd843ebdded79ce2d82ca
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-01-31 22:45:23 +0000

    Added some extra critera

--- src/app.js
+++ src/app.js
@@ -2,19 +2,30 @@ const yargs = require("yargs");
 const { Movie, movieArr } = require("./utils");
 
 const app = (args) => {
-    if(args.add){
-        const movie = new Movie(args.movie, args.actor)
-        movie.add()
-        console.log(movieArr);
-    } else if (args.addMany) {
-        const movie1 = new Movie(args.movie1, args.actor1)
-        const movie2 = new Movie(args.movie2, args.actor2)
-        movie.add()
-        console.log(movieArr);
-    } else {
-        console.log("Incorrect Command");
-    }
-}
+  if (args.add) {
+    const movie = new Movie(args.movie, args.actor, args.runtime, args.year);
+    movie.add();
+    console.log(movieArr);
+  } else if (args.addMany) {
+    const movie1 = new Movie(
+      args.movie1,
+      args.actor1,
+      args.runtime1,
+      args.year1
+    );
+    const movie2 = new Movie(
+      args.movie2,
+      args.actor2,
+      args.runtime2,
+      args.year2
+    );
+    movie1.add();
+    movie2.add();
+    console.log(movieArr);
+  } else {
+    console.log("Incorrect Command");
+  }
+};
 
 app(yargs.argv);
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 const movieArr = [];
 
 class Movie{
-        constructor(title, actor){
+        constructor(title, actor, runtime, year){
             this.title = title;
             this.actor = actor;
+            this.runtime = runtime;
+            this.year = year;
         }
         add(){
             movieArr.push(this);
